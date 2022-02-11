d33ba2a37d3e0222d383386e08a9c894a3188495
Alexander Kruman <akruman@gmail.com>
2022-02-12 00:03:00 +0200

    fix: read_word

--- iolib.asm
+++ iolib.asm
@@ -80,11 +80,98 @@ print_int: ;Output a signed 8-byte integer in decimal format.
     call print_uint
     ret
 
+read_char: ;Read one character from stdin and return it. If the end of input stream occurs, return 0.
+    mov rdx,1 ; count=1
+    mov rax,0 ; read
+
+    push rdi
+    lea rsi, [rsp-1] ; stack grows down
+    mov rdi,0 ; stdin
+    syscall ; end of stream rax rets 0
+    test rax, rax
+    jz .end
+
+    xor rax,rax
+    mov al, [rsp-1] ; return in rax
+.end:
+    pop rdi
+    ret   
+
+is_whitespace: ; if dil is white space ZF=1
+    cmp dil, 0x20
+    jz .end
+    cmp dil, 0x10
+    jz .end
+    cmp dil, 0x09
+.end:
+    ret
+
+is_eof: ; if dil is white space ZF=1
+    cmp dil, 0
+    ret
+
+; why i need twice ^d ?
+read_word: ; (buffer* rdi, size rsi)
+    ;Accepts a buffer address and size as arguments. 
+    ;Reads next word from stdin (skipping whitespaces into buffer). 
+    ;Stops and returns 0 if word is too big for the buffer specified; otherwise returns a buffer address.
+    ;This function should null-terminate the accepted string.
+
+    push r12
+    push r13
+    push rdi
+
+    test rsi,rsi
+    jz .err
+
+    mov r12, rdi ; pbuf
+    lea r13, [rdi + rsi -1 ] ; pbuf+size-1, the last addr to write
+    mov byte[r12], 0 ; null terminate
+
+.whitespace:
+    call read_char
+    mov dil, al
+    call is_whitespace
+    jz .whitespace
+    call is_eof
+    jz .retbuf
+
+.write_al:
+    ; al has valid char to write
+    cmp r12,r13
+    je .err
+    mov byte[r12], al
+    inc r12
+    mov byte[r12], 0
+    call read_char
+    mov dil, al
+    call is_whitespace
+    jz .retbuf
+    call is_eof
+    jz .retbuf
+    jmp .write_al
+
+.err:
+    xor rax,rax ; ret 0 on err
+    pop rdi
+    pop r13
+    pop r12
+    ret
+.retbuf:
+    pop rdi
+    mov rax,rdi
+    pop r13
+    pop r12
+    ret
+
+
 section .data
     s1: db "asdf",0
-    s2: 
+    s2: times 8 db 0
+    s3:
 section .text
 _start:
+    jmp .test1
     mov rdi, s1
     call print_string
     mov rdi, 65
@@ -98,6 +185,23 @@ _start:
 
     mov rdi, -123456
     call print_int
+
+    call read_char
+    mov rdi,rax
+    call print_char ; print back the char read
+
+    call print_newline
+
+.test1:
+    mov rdi, s2
+    mov rsi, s3-s2
+    call read_word
+    test rax,rax
+    jz .next
+    mov rdi,s2
+    call print_string
+.next:
+    call print_newline
 
     mov rdi, 1
     call exit
--- readme.txt
+++ readme.txt
@@ -25,3 +25,21 @@ mov rax, [rbx + 4* rcx + 9]
 lea rax, [rbx + rbx * 4]     ; rax = rbx * 5
 
 https://cs.brown.edu/courses/cs033/docs/guides/x64_cheatsheet.pdf
+
+8-byte register Bytes 0-3 Bytes 0-1 Byte 0
+%rax %eax %ax %al
+%rcx %ecx %cx %cl
+%rdx %edx %dx %dl
+%rbx %ebx %bx %bl
+%rsi %esi %si %sil
+%rdi %edi %di %dil
+%rsp %esp %sp %spl
+%rbp %ebp %bp %bpl
+%r8 %r8d %r8w %r8b
+%r9 %r9d %r9w %r9b
+%r10 %r10d %r10w %r10b
+%r11 %r11d %r11w %r11b
+%r12 %r12d %r12w %r12b
+%r13 %r13d %r13w %r13b
+%r14 %r14d %r14w %r14b
+%r15 %r15d %r15w %r15b
