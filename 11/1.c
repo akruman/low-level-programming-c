e4bc77437338db62a0c2a531f34af418c7e1d6a0
Alexander Kruman <akruman@gmail.com>
2022-03-26 23:21:37 +0300

    fix: 11

--- 11/1.c
+++ 11/1.c
@@ -1,7 +1,12 @@
 #include <inttypes.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include <stdio.h> // BUFSIZ
 #include <stdlib.h>
- 
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 struct List {
     struct Node *first;
     struct Node *last;
@@ -57,12 +62,96 @@ struct List* map_mut(struct List *list, int (*f)(int)) {
     return list;
 }
 
+// return list with n elements: [s, f(s), f(f(s)), ..]
+struct List* iterate(int s, size_t n, int (*f)(int)) {
+    struct List* ret = List_init();
+    if(ret){
+        for(;n>0 && List_append(ret, s);n--){  
+            s = f(s);
+        }
+    }
+    return ret;
+}
 
 int fold1(struct List *list, int (*f_val_acc)(int, int), int acc) {
     struct Node *it;
     for(it = list->first; it; it=it->next) acc = f_val_acc(it->val,acc);
     return acc;
 }
+
+bool save(struct List* list, const char* filename) {
+    bool ret = false;
+    FILE *f = fopen(filename, "wt+");
+    if(f){
+        struct Node *it;
+        for(it = list->first; it; it=it->next){
+            if(0>fprintf(f,"%d ",it->val)) goto err1;
+        }
+        ret = true;
+err1:
+        fclose(f);    
+    }
+    return ret;
+}
+
+bool load(struct List** list, const char* filename){
+    int val, n;
+    *list = List_init();
+    FILE *f = fopen(filename, "rt");
+    if(f){
+        while((n=fscanf(f,"%d",&val)) && n!=EOF && n>0 && List_append(*list, val));
+        fclose(f);
+        return true;
+    }
+    return false;
+}
+
+bool serialize(struct List* list, const char* filename){
+    bool ret = false;
+    FILE *f = fopen(filename, "wb+");
+    if(f){
+        struct Node *it;
+        for(it = list->first; it; it=it->next){   
+            if(1!=fwrite(&it->val, sizeof(it->val), 1, f)) goto err1;
+        }
+        ret = true;
+err1:
+        fclose(f);    
+    }
+    return ret;
+}
+bool deserialize(struct List** list, const char* filename){
+    int val, n;
+    *list = List_init();
+    FILE *f = fopen(filename, "rb");
+    if(f){      
+        while((n=fread(&val,sizeof(val), 1, f)) && n==1 && List_append(*list, val));
+        fclose(f);
+        return true;
+    }
+    return false;
+}
+
+void List_clean(struct List* list){
+    if(list){
+        struct Node *it = list->first, *tmp;
+        while(it){
+            tmp = it;
+            it = it->next;
+            free(tmp);
+        }
+        list->first = list->last = NULL;
+    }
+}
+
+void List_free(struct List** list){
+    if(list && *list){
+        List_clean(*list);
+        free(*list);
+        *list = NULL;
+    }
+}
+
 
 void print_int_space(int val){printf("%d ",val);}
 void print_int_cr(int val){printf("%d\n",val);}
@@ -72,6 +161,7 @@ int sum(int x, int acc) {return x+acc;}
 int min(int x, int acc) {return (x<acc)?x:acc;}
 int max(int x, int acc) {return (x>acc)?x:acc;}
 int mod2(int x){return x%2;}
+int mul2(int x){return x*2;}
 
 int main(){
     
@@ -99,6 +189,27 @@ int main(){
     map_mut(&list, mod2);
     foreach(&list, &print_int_space);
     puts("");
+    struct List *list4 = iterate(1,10,mul2);
+    foreach(list4, &print_int_space);
+    puts("");
+
+    save(&list, "1.txt");
+    struct List *list5;
+    load(&list5, "1.txt");
+    foreach(list5, &print_int_space);
+    puts("");
+ 
+    serialize(&list, "1.bin");
+    struct List *list6;
+    deserialize(&list6, "1.bin");
+    foreach(list6, &print_int_space);
+    puts("");
+
+    List_clean(&list);
+    List_free(&list2);
+    List_free(&list3);
+    List_free(&list4);
+    List_free(&list5);
 
     return 0;
 }
